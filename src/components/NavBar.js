6df1a91a511009a473d7f7f653e2be272c82b281
ngborrelli <ngborrelli@gmail.com>
2022-08-28 22:44:07 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import estilos from '../assets/css/estilos.css';
+import logo from '../assets/images/porsche.png';
+
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+        <img src={logo} alt="logo"></img>
+        <h1>Autopartes Borrelli</h1>
+        <ul className="menu">
+            <li>
+                <a href="#">Todo</a>
+            </li>
+            <li>
+            <a href="#">Filtros</a>
+            </li>
+            <li>
+                <a href="#">Frenos</a>
+            </li>
+            <li>
+            <a href="#">Inyeccion</a>
+            </li>
+            <li>
+            <a href="#">Suspension</a>
+            </li>
+
+        </ul>
+
+        <button onClick="alert('asdsad')">Login</button>
+    </nav>
+  )
+}
+
+export default NavBar
