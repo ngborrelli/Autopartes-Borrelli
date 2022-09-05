26818daf9aebc28dd0062c50f6196a839aa19a53
ngborrelli <ngborrelli@gmail.com>
2022-09-05 17:34:26 -0300

    Item counter

---
+++ src/components/ItemCount.js
@@ -0,0 +1,43 @@
+import React, { Component, useState } from 'react';
+import estilos from '../assets/css/estilos.css';
+
+const ItemCount = (prop) => {
+
+    let stock = prop.stock;
+
+    const addItem = () => {
+        setItemCounter(itemCounter < prop.stock ? itemCounter + 1 : itemCounter); 
+    }
+
+    const subItem = () => {
+        setItemCounter(itemCounter > 0 ? itemCounter - 1 : itemCounter); 
+    }
+
+    const agregarCarrito = (cantidad) => {
+        if (cantidad > prop.stock) {
+            console.log("No hay stock suficiente");
+        }
+        else {
+            if (cantidad > 0) {
+            console.log("Se agregaron " + cantidad + " items")
+            }
+        }
+    }
+
+    const [itemCounter, setItemCounter] = useState(0);
+
+  return (
+    <div className="itemCounter">
+        <h2>Item</h2>
+        <div className="cajaBotones">
+            <button onClick={subItem}>-</button>
+            <span>{itemCounter}</span>
+            <button onClick={addItem}>+</button>
+        </div>
+
+        <button onClick={() => {agregarCarrito(itemCounter)}}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import estilos from '../assets/css/estilos.css';
+import ItemCount from '../components/ItemCount';
 import logo from '../assets/images/porsche.png';
 
 const ItemListContainer = (prop) => {
   return (
-    <div>
+    <div className="itemListContainer">
         <h1>Bienvenido, {prop.title}</h1>
+        <ItemCount stock="10" />
     </div>
   )
 }
