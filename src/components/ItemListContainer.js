7e95896b184740d5007f26dc2abeb5d65eae6809
ngborrelli <ngborrelli@gmail.com>
2022-10-19 19:38:10 -0300

    Entrega Final

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import estilos from '../assets/css/estilos.css';
-import ItemCount from '../components/ItemCount';
 import ItemList from '../components/ItemList.js';
-import logo from '../assets/images/porsche.png';
 
 const ItemListContainer = () => {
   return (
